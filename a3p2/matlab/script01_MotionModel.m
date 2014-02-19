fedf5f3896823b1e1c211bfc5acf6ab7cd9e15f0
Clayton Stout <stoutc.gatech@gmail.com>
2014-02-19 17:02:32 -0500

    Created our environment virtually with Square Map utility

--- a3p2/matlab/script01_MotionModel.m
+++ a3p2/matlab/script01_MotionModel.m
@@ -3,22 +3,33 @@
 
 path(path,'threedee') 
 
+A = [-1.5, .5, .5588, .3556
+-1.5, -.5, .5588, .3556
+-.9412, .8556, .5588, .3556
+-.9412, -.8556, .5588, .3556
+-.3824, .8556, .5588, .3556
+-.3824, -.8556, .5588, .3556
+-.5588, 0, .5588, .3556
+0, 0, .5588, .3556];
+
+map = SquareMap(A, 2.5);
+
 %% Create a map
-A = [
-    -3,2,6,1
-    -3,-4,4,2
-    3,-4,1,6
-    ]
-map=SquareMap(A)
+%A = [
+%    -3,2,6,1
+%    -3,-4,4,2
+%    3,-4,1,6
+%    ]
+%map=SquareMap(A)
 
 %% and a robot with noisy odometry
 V=diag([0.01, 0.1*pi/180].^2)
 veh=Differential(V)
-veh.add_driver(DeterministicPath('log-example2.txt'));
+veh.add_driver(DeterministicPath('NoSensorLog.txt'));
 
 %% and then a sensor with noisy readings
 W=0.05^2;
-sensor = RangeSensor(veh,map, W,'log-example2.txt')
+sensor = RangeSensor(veh,map, W,'NoSensorLog.txt')
 
 %% define two covariances for random noise Q and L (hmmm!)
 % For Q, use the uncertainly estimates from A2!
